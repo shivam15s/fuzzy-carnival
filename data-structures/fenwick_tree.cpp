792c69557fdfbd71d84ef6a15498a22e2876cb0d
shivam15s <shivam.sahni@iitgn.ac.in>
2020-05-21 01:36:42 +0530

    fenwick-tree

--- data-structures/fenwick_tree.cpp
+++ data-structures/fenwick_tree.cpp
@@ -34,18 +34,3 @@ struct FT {
 		return pos;
 	}
 };
-
-int main(){
-
-#ifndef ONLINE_JUDGE 
-    freopen("input.txt", "r", stdin); 
-    freopen("output.txt", "w", stdout); 
-#endif
-    
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    
-
-    // cerr<<"\nTime elapsed: "<<1000*clock()/CLOCKS_PER_SEC<<"ms\n";
-    return 0;
-}
